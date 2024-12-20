6e43d7da174d42be332cbb23490cf6688ae26c8a
pelinnecee <pelinece03@gmail.com>
2024-12-20 10:08:32 +0300

    Unfollowing and See Profile problems fixed,showed at Explore Users page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,17 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :saved_posts_relationships, class_name: 'SavedPost', foreign_key: 'user_id'
   has_many :saved_posts, through: :saved_posts_relationships, source: :post
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :active_follows, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
-  has_many :passive_follows, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
-  has_many :following, through: :active_follows, source: :followed
-  has_many :followers, through: :passive_follows, source: :follower
+
+
+
+  # Relationships for easy access
+
 
   # Devise Modules
   devise :database_authenticatable, :registerable,
@@ -41,8 +46,8 @@ class User < ApplicationRecord
     following << user unless self == user || following?(user)
   end
 
-  def unfollow(user)
-    active_follows.find_by(followed_id: user.id)&.destroy
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id)&.destroy
   end
 
   def following?(user)
