36956294e966fd578ccedfa9b9a47b3d4a6d4dbf
osman <osmanmeddimeddi@gmail.com>
2024-12-26 17:39:37 +0300

    Hotfix: [osman]

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,52 @@
-require "test_helper"
+require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = posts(:one) # Assuming you have a fixture for posts
+  end
+
+  test "should get index" do
+    get posts_url
+    assert_response :success
+    assert_not_nil assigns(:posts)
+  end
+
+  test "should get new" do
+    get new_post_url
+    assert_response :success
+  end
+
+  test "should create post" do
+    assert_difference('Post.count') do
+      post posts_url, params: { post: { title: 'Test Post', content: 'This is a test content.' } }
+    end
+
+    assert_redirected_to post_url(Post.last)
+  end
+
+  test "should show post" do
+    get post_url(@post)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_post_url(@post)
+    assert_response :success
+  end
+
+  test "should update post" do
+    patch post_url(@post), params: { post: { title: 'Updated Title', content: 'Updated content.' } }
+    assert_redirected_to post_url(@post)
+    @post.reload
+    assert_equal 'Updated Title', @post.title
+    assert_equal 'Updated content.', @post.content
+  end
+
+  test "should destroy post" do
+    assert_difference('Post.count', -1) do
+      delete post_url(@post)
+    end
+
+    assert_redirected_to posts_url
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,51 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_show_url
+  setup do
+    @user = users(:one) # Assuming you have a fixture for users
+  end
+
+  test "should get index" do
+    get users_url
     assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  test "should get new" do
+    get new_user_url
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference('User.count') do
+      post users_url, params: { user: { name: 'Test User', email: 'test@example.com', password: 'password' } }
+    end
+
+    assert_redirected_to user_url(User.last)
+  end
+
+  test "should show user" do
+    get user_url(@user)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_user_url(@user)
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch user_url(@user), params: { user: { name: 'Updated Name' } }
+    assert_redirected_to user_url(@user)
+    @user.reload
+    assert_equal 'Updated Name', @user.name
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user)
+    end
+
+    assert_redirected_to users_url
   end
 end
