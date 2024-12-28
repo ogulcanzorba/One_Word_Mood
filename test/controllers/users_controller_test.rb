3206f2788736a53d7092979f48761242441a6ff3
Ancienty <furkankck2003@gmail.com>
2024-12-28 18:40:11 +0300

    Changes for controller tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,29 +11,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # REMOVED test "should get new"
-
-  test "should create user" do
-    assert_difference("User.count", 1) do
-      post users_url, params: {
-        user: {
-          name: "TestUser",
-          handle: "testing1",
-          email: "unique#{SecureRandom.hex}@example.com",
-          password: "password",
-          password_confirmation: "password"
-        }
-      }
-    end
-    assert_redirected_to user_url(User.last)
-  end
-
   test "should show user" do
     get user_url(@user)
     assert_response :success
   end
-
-  # REMOVED test "should get edit"
 
   test "should update user" do
     patch user_url(@user), params: {
