4a626310bb8c57db38a30f0e31089d079090f98b
Ancienty <furkankck2003@gmail.com>
2024-12-29 15:35:31 +0300

    Changes on the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Validations
-  validates :name, presence: true
+  validates :handle, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   # Associations
